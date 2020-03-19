916629cc1016a3e01979cb774913da7ff609d13a
nbon12 <nbon12@users.noreply.github.com>
2020-03-18 21:33:57 -0400

    connect database

--- character_builder/character_builder/settings.py
+++ character_builder/character_builder/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = 'character_builder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PASSWORD': 'postgres',
+        'PORT': 5432
+        
     }
 }
 
